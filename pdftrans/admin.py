f958063d040db9fe6bfbe1f421a845341e04bceb
MaxSpacer <max_spacer@mail.ru>
2019-07-02 01:53:18 +0300

    1,92

--- pdftrans/admin.py
+++ pdftrans/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Order
-# Register your models here.
+from .models import Order, Adress
+
+class AdressInline(admin.TabularInline):
+    model = Adress# Register your models here.
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Order._meta.fields]
     readonly_fields = ('barcode','qrcode','order_number')
+    inlines = [AdressInline]
 admin.site.register(Order, OrderAdmin)
--- pdftrans/models.py
+++ pdftrans/models.py
@@ -10,10 +10,12 @@ from barcode import generate
 from barcode.writer import ImageWriter
 
 
+
 class Order(models.Model):
-    customer_name = models.CharField(verbose_name="имя инженера", max_length=64, blank=False, null=True)
-    customer_adress = models.TextField(verbose_name="адрес помещения", blank=False, null=True)
-    customer_data = models.DateTimeField(auto_now_add=False, auto_now=False)
+    engineer_name = models.CharField(verbose_name="имя инженера", default="Клименко М.В.", max_length=64, blank=False, null=True)
+    customer_name = models.CharField(verbose_name="имя начальника отделения", default="Панин В.Э.", max_length=64, blank=False, null=True)
+    object_list = models.CharField(verbose_name="cписок объектов", max_length=64, blank=False, null=True)
+    customer_data = models.DateTimeField(verbose_name="дата документа", auto_now_add=False, auto_now=False)
     image = models.ImageField('схема помещения',upload_to='schema_images/')
     image_2 = models.ImageField('таблица экспликации',upload_to='explications_images/')
     barcode = models.ImageField(blank=True, null=True, upload_to='barcode/')
@@ -61,5 +63,24 @@ class Order(models.Model):
         verbose_name_plural = 'Заказы'
 
     def save(self, *args, **kwargs):
-        self.generate_qr_bar_code()
+        if self.order_number:
+            pass
+        else:
+            self.generate_qr_bar_code()
         super(Order, self).save(*args, **kwargs)
+
+
+class Adress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.SET_DEFAULT, null=True, default=None, verbose_name = 'статус заказа')
+    subject_rf = models.CharField(verbose_name="субъект РФ", max_length=64, blank=False, null=True)
+    rayon = models.CharField(verbose_name="Район", max_length=64, blank=False, null=True)
+    mun_type = models.CharField(verbose_name="Муниципальное образование тип", max_length=64, blank=False, null=True)
+    mun_name = models.CharField(verbose_name="Муниципальное образование наименование ", max_length=64, blank=False, null=True)
+    city_type = models.CharField(verbose_name="Населенный пункт тип", max_length=64, blank=False, null=True)
+    city_name = models.CharField(verbose_name="Населенный пункт наименование", max_length=64, blank=False, null=True)
+    street = models.CharField(verbose_name="Улица (проспект, переулок и т. д.)", max_length=64, blank=False, null=True)
+    house_number = models.CharField(verbose_name="Номер дома", max_length=64, blank=False, null=True)
+    corpus_number = models.CharField(verbose_name="Номер корпуса", max_length=64, blank=False, null=True)
+    build_number = models.CharField(verbose_name="Номер строения", max_length=64, blank=False, null=True)
+    corpus_number = models.CharField(verbose_name="Номер помещения (квартиры)", max_length=64, blank=False, null=True)
+    another_places = models.CharField(verbose_name="Иное описание местоположения", max_length=64, blank=False, null=True)
