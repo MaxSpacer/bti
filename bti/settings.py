7924e65c2188b25f3b864fddce556b77e24abcb3
MaxSpacer <max_spacer@mail.ru>
2019-07-22 17:41:55 +0300

    d8

--- bti/settings.py
+++ bti/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static', 'media'),
 os.path.join(BASE_DIR, 'static', 'static_dev'),
 )
-#uncomment
+# uncomment
 try:
     from .settings_prod import *
 except:
