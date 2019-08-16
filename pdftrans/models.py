fc7b4d06afb7558974d348ea7ac8654a3958e460
MaxSpacer <max_spacer@mail.ru>
2019-08-17 02:18:15 +0300

    redep6

--- pdftrans/models.py
+++ pdftrans/models.py
@@ -15,24 +15,23 @@ from django.utils import timezone
 from settings_pdftrans.models import *
 
 def get_doc_type_choices():
-    # DOC_TYPE_CHOICES = [(str(e.docs_type), e.docs_type) for e in DocType.objects.all()]
-    # return DOC_TYPE_CHOICES
-    pass
+    DOC_TYPE_CHOICES = [(str(e.docs_type), e.docs_type) for e in DocType.objects.all()]
+    return DOC_TYPE_CHOICES
 # self._meta.get_field('doc_type').choices = get_doc_type_choices()
 # self._meta.get_field('doc_type').default = DocType.objects.filter().first()
 
 def get_type_object_choices():
-    # TYPE_OBJECT_CHOICES = [(str(e.objects_type), e.objects_type) for e in TypeObject.objects.all()]
-    # return TYPE_OBJECT_CHOICES
+    TYPE_OBJECT_CHOICES = [(str(e.objects_type), e.objects_type) for e in TypeObject.objects.all()]
+    return TYPE_OBJECT_CHOICES
 # self._meta.get_field('type_object').choices = get_type_object_choices()
 # self._meta.get_field('type_object').default = TypeObject.objects.filter().first()
-    pass
+
 def get_name_object_choices():
-    # NAME_OBJECT_CHOICES = [(str(e.objects_name), e.objects_name) for e in NameObject.objects.all()]
-    # return NAME_OBJECT_CHOICES
+    NAME_OBJECT_CHOICES = [(str(e.objects_name), e.objects_name) for e in NameObject.objects.all()]
+    return NAME_OBJECT_CHOICES
 # self._meta.get_field('name_object').choices = get_name_object_choices()
 # self._meta.get_field('name_object').default = NameObject.objects.filter().first()
-    pass
+
 class Order(models.Model):
     order_number = models.PositiveIntegerField(blank=True, null=True, default = 0)
     uploaded_pdf = models.FileField(verbose_name="Исходный документ(pdf)", upload_to='uploaded_pdf/%Y/%m/%d/', blank=True, null=True)
