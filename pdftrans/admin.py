f5913a73efb55cb142505e7bc159595b9d11d583
MaxSpacer <max_spacer@mail.ru>
2019-08-17 13:39:49 +0300

    red9.1

--- pdftrans/admin.py
+++ pdftrans/admin.py
@@ -8,7 +8,8 @@ class OrderImageInline(admin.TabularInline):
     # def full_pdf_url(self, obj):
     #     return format_html("<a href='{url}'>{url}</a>", url=obj.fullpdf_url_staff)
     # readonly_fields = ['full_pdf_url', 'fullpdf_url_staff', 'order_image',]
-    readonly_fields = ['fullpdf_url_staff', 'order_image']
+    # readonly_fields = ['fullpdf_url_staff', 'order_image']
+    readonly_fields = [field.name for field in OrderImage._meta.fields]
     max_num = 1
 
 class AdressInline(admin.StackedInline):
