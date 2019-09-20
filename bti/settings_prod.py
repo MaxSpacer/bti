a6d92c72725b9c5e5aa4590dc0d7e9fbe2cdc060
MaxSpacer <max_spacer@mail.ru>
2019-09-20 12:16:39 +0300

    for dep

--- bti/settings_prod.py
+++ bti/settings_prod.py
@@ -12,13 +12,13 @@ ALLOWED_HOSTS = ['167.71.54.163', 'localhost', '127.0.0.1']
 #         'PORT': '',                      # Set to empty string for default.
 #     }
 # }
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'localhost'
-# EMAIL_PORT = '1025'
-# EMAIL_HOST = 'smtp.yandex.ru'
-# EMAIL_PORT = 465
-# EMAIL_HOST_USER = "info@doxaih.ru"
-# EMAIL_HOST_PASSWORD = "In123456"
-# EMAIL_USE_SSL = True
-# SERVER_EMAIL = EMAIL_HOST_USER
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '1025'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "info@doxaih.ru"
+EMAIL_HOST_PASSWORD = "In123456"
+EMAIL_USE_SSL = True
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
